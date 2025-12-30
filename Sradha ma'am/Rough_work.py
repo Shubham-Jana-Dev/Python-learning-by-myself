4999e50bb6867012a60ea76919f6258836904816
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-30 19:00:08 +0530

    fact: learn about enumerate() functions

--- Sradha ma'am/Rough_work.py
+++ Sradha ma'am/Rough_work.py
@@ -10,3 +10,26 @@ while i < len(t):
         print("not found")
     i+=1
 '''
+# print the Following elements of the list using for loop.
+'''
+my_list = [1,4,9,16,25,36,49,64,81,100]
+for i in my_list:
+    print(i) 
+'''
+# find out a given number from the following list.
+"""
+my_list = [1,4,9,16,25,36,49,64,81,100,49]
+t = 49
+for ind in range(len(my_list)):
+    if (my_list[ind] == t):
+        print(f"at index number,{ind} we can find",t)
+"""
+# another way..
+"""
+my_list = [1,4,9,16,25,36,49,64,81,100,49]
+t = 49
+for ind, i in enumerate(my_list):
+    if (i == t):
+        print(f"at index number,{ind} we can find",t)
+"""
+
