a6e59dbacb58af297551e3a6cdb59e3aacc40488
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-30 19:13:10 +0530

    refactor: make it more robust, use 'in' keyword

--- Coding_challenge_from_friends/evening_chllenge.py
+++ Coding_challenge_from_friends/evening_chllenge.py
@@ -6,9 +6,9 @@ Problem #50: The "Input" Validator
 """
 def input_Validator():
     x = input("Enter the Situation: ")
-    if x == "Active" or x == "Aalid":
+    if "Active" in x or "Aalid" in x:
         print("System Clean. Operations running smoothly. :)")
-    elif x == "Error" or x == "Blocked":
+    elif "Error" in x or  "Blocked" in x:
         print("Access Denied: Re-routing to Library Server. :(")
     else:
         print("Neutral State please provide a standard command like (\"Active\" or \"valid\" or \"Error\" or \"Blocked\").")
